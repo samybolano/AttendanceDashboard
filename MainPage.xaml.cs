6e71be051f577c5174867df383ebe9d3ae4b521f
samuelbolano <samuel.bolano@metalteco.com>
2025-05-22 05:42:06 -0500

    add V_GraficaAsistenciasMeses

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using AttendanceDashboard.Models;
+using System.Collections.ObjectModel;
 
 namespace AttendanceDashboard
 {
---
+++ Models/V_GraficaAsistenciasMeses.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceDashboard.Models
+{
+    public class V_GraficaAsistenciasMeses
+    {
+        public string Año { get; set; }
+        public string NumeroMes { get; set; }
+        public string Mes { get; set; }
+        public int TotalAsistencias { get; set; }
+    }
+}
